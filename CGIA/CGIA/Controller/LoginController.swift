bff3cab1e07b52a7daec69aa4aa8df993464b9d0
Pedro Farina <pedrogfarina@gmail.com>
2019-09-24 19:52:46 -0300

    Criando projeto

---
+++ CGIA/CGIA/Controller/LoginController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CGIA
+//
+//  Created by Pedro Giuliano Farina on 24/09/19.
+//  Copyright © 2019 Pedro Giuliano Farina. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    @IBOutlet weak var txtUsername: UITextField!
+    
+    @IBAction func loginTap(_ sender: UIButton) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
