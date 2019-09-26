d480f068f67373a1db6f56b884ae94b171ec15bc
Galdineris <galdineris@gmail.com>
2019-09-25 22:06:37 -0300

    reinforçando o gitignore 2/2

---
+++ CGIA/CGIA/Model/ServerManager.swift
@@ -0,0 +1,33 @@
+//
+//  ServerManager.swift
+//  CGIA
+//
+//  Created by Pedro Giuliano Farina on 24/09/19.
+//  Copyright © 2019 Pedro Giuliano Farina. All rights reserved.
+//
+
+import Foundation
+
+public class ServerManager {
+
+    // MARK: Login
+    public func authenticateLogin(username: String, completionHandler: (LoginAnswer) -> Void) {
+        completionHandler(.successful(Aluno(instituicao: Instituicao())))
+    }
+
+    // MARK: Fetchs
+
+    // MARK: Singleton Properties
+    private init() {
+    }
+
+    class func shared() -> ServerManager {
+        return sharedServerManager
+    }
+
+    private static var sharedServerManager: ServerManager = {
+        let manager = ServerManager()
+        
+        return manager
+    }()
+}
