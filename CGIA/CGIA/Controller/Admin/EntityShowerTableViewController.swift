d053dcfee99ea9bd5b5de721c66ea5f3a1a78bce
Galdineris <galdineris@gmail.com>
2019-11-18 00:32:11 -0300

    SearchBar Implementation

--- CGIA/CGIA/Controller/Admin/EntityShowerTableViewController.swift
+++ CGIA/CGIA/Controller/Admin/EntityShowerTableViewController.swift
@@ -11,12 +11,23 @@ import UIKit
 public class EntityShowerTableViewController: UITableViewController {
     public var profile: Any?
     private var data: [Displayable] = []
+    private var filteredData: [Displayable] = []
     private var selected: Displayable?
+    var resultSearchController = UISearchController()
 
     public override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(self, selector: #selector(updateData), name:
             Notification.Name(NotifName.dataUpdated.rawValue), object: nil)
+        resultSearchController = ({
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.searchBar.sizeToFit()
+            controller.obscuresBackgroundDuringPresentation = false
+            definesPresentationContext = true
+            tableView.tableHeaderView = controller.searchBar
+            return controller
+        })()
     }
 
     @objc func updateData() {
@@ -33,30 +44,43 @@ public class EntityShowerTableViewController: UITableViewController {
         guard let profile = profile else {
             fatalError("Tipo era nulo")
         }
-        switch profile {
-        case is CompleteInstructor.Type:
-            data = ServerManager.shared().professores
-        case is CompleteSubject.Type:
-            data = ServerManager.shared().disciplinas
-        case is CompleteClassroom.Type:
-            data = ServerManager.shared().turmas
-        case is CompleteStudent.Type:
-            data = ServerManager.shared().alunos
-        default:
-            fatalError("Não houve uma tipagem esperada para mostrar os dados")
+        var retorno: Int = 0
+        if resultSearchController.isActive {
+            retorno = filteredData.count
+        } else {
+            switch profile {
+            case is CompleteInstructor.Type:
+                data = ServerManager.shared().professores
+            case is CompleteSubject.Type:
+                data = ServerManager.shared().disciplinas
+            case is CompleteClassroom.Type:
+                data = ServerManager.shared().turmas
+            case is CompleteStudent.Type:
+                data = ServerManager.shared().alunos
+            default:
+                fatalError("Não houve uma tipagem esperada para mostrar os dados")
+            }
+            retorno = data.count
         }
-        return data.count
+        return retorno
     }
 
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = data[indexPath.row].displayName
-
+        if resultSearchController.isActive {
+            cell.textLabel?.text = filteredData[indexPath.row].displayName
+        } else {
+            cell.textLabel?.text = data[indexPath.row].displayName
+        }
         return cell
     }
 
     public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selected = data[indexPath.row]
+        if resultSearchController.isActive {
+            selected = filteredData[indexPath.row]
+        } else {
+            selected = data[indexPath.row]
+        }
         performSegue(withIdentifier: "selected", sender: self)
     }
 
@@ -70,5 +94,15 @@ public class EntityShowerTableViewController: UITableViewController {
             let cont = navCont.topViewController as? EntityAdderController {
             cont.profile = self.profile
         }
+    }
+}
+
+extension EntityShowerTableViewController: UISearchResultsUpdating {
+    public func updateSearchResults(for searchController: UISearchController) {
+        filteredData.removeAll(keepingCapacity: false)
+        filteredData = data.filter({ (entry) -> Bool in
+            return entry.displayName.lowercased().contains((searchController.searchBar.text ?? "").lowercased())
+        })
+        self.tableView.reloadData()
     }
 }
